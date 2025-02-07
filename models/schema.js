af005730586a2d41b255593705e77124e840df1e
Jebakumar <jebakumar.6208@gmail.com>
2025-02-07 14:27:28 +0530

    done

---
+++ models/schema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  roles: { type: [String], default: ['user'] },
+  profile: {
+    firstName: String,
+    lastName: String,
+    age: Number,
+  },
+  lastLogin: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('User', userSchema);
